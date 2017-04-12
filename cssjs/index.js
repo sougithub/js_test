9d89cd28b4d6a34aa84b80337222e370d5038f3f
sougithub <sousowsou@gmail.com>
2017-04-12 20:56:01 +0900

    first commit

---
+++ cssjs/index.js
@@ -0,0 +1,31 @@
+var myGrades = [93, 95, 88, 0, 55, 91];
+
+var myGradesCalculate = (function () {
+
+  // Keep this variable private inside this closure scope
+  // var myGrades = [93, 95, 88, 0, 55, 91];
+
+  // Expose these functions via an interface while hiding
+  // the implementation of the module within the function() block
+
+  return {
+    average: function() {
+      var total = myGrades.reduce(function(accumulator, item) {
+        return accumulator + item;
+        }, 0);
+
+      return'Your average grade is ' + total / myGrades.length + '.';
+    },
+
+    failing: function() {
+      var failingGrades = myGrades.filter(function(item) {
+          return item < 70;
+        });
+
+      return 'You failed ' + failingGrades.length + ' times.';
+    }
+  }
+})();
+
+// myGradesCalculate.failing(); // 'You failed 2 times.'
+// myGradesCalculate.average(); // 'Your average grade is 70.33333333333333.'
